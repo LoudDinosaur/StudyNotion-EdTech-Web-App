1e9cbefef4a9642a39acde6f3b0a9ea92ef7c986
LoudDinosaur <kavishaman11@gmail.com>
2024-12-11 15:13:16 +0530

    Added Instructor Chart

--- src/components/core/Dashboard/InstructorDashboard/InstructorChart.jsx
+++ src/components/core/Dashboard/InstructorDashboard/InstructorChart.jsx
@@ -1,74 +1,82 @@
-import React, { useState } from 'react'
-
-import {Chart, registerables} from "chart.js"
-import  {Pie} from "react-chartjs-2"
+import React, { useState } from "react";
+import { Chart, registerables } from "chart.js";
+import { Pie } from "react-chartjs-2";
 
 Chart.register(...registerables);
 
-const InstructorChart = ({courses}) => {
+export default function InstructorChart({ courses }){
+  const [currChart, setCurrChart] = useState("students");
 
-    const [currChart, setCurrChart] = useState("students");
-
-    //function to generate random colors
-    const getRandomColors = (numColors) => {
-        const colors = [];
-        for(let i=0; i<numColors; i++){
-            const color = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random()*256)}, ${Math.floor(Math.random()*256)})`
-            colors.push(color);
-        }
-        return colors;
+  //function to generate random colors
+  const generateRandomColors = (numColors) => {
+    const colors = [];
+    for (let i = 0; i < numColors; i++) {
+      const color = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(
+        Math.random() * 256
+      )}, ${Math.floor(Math.random() * 256)})`;
+      colors.push(color);
     }
+    return colors;
+  };
 
-    //create data for chart displaying student info
-    const chartDataForStudents = {
-        labels: courses.map((course) => course.courseName),
-        datasets: [
-            {
-                data: courses.map((course) => course.totalStudentsEnrolled),
-                backgroundColor: getRandomColors(courses.length),
-            }
-        ]
-    }
+  //create data for chart displaying student info
+  const chartDataForStudents = {
+    labels: courses.map((course) => course.courseName),
+    datasets: [
+      {
+        data: courses.map((course) => course.totalStudentsEnrolled),
+        backgroundColor: generateRandomColors(courses.length),
+      },
+    ],
+  };
 
-    //create data for chart displaying income info
-    const chartDataForIncome = {
-        labels: courses.map((course) => course.courseName),
-        datasets: [
-            {
-                data: courses.map((course) => course.totalAmountGenerated),
-                backgroundColor: getRandomColors(courses.length),
-            }
-        ]
-    }
-    //create options
-    const options = {
-
-    }
+  //create data for chart displaying income info
+  const chartDataForIncome = {
+    labels: courses.map((course) => course.courseName),
+    datasets: [
+      {
+        data: courses.map((course) => course.totalAmountGenerated),
+        backgroundColor: generateRandomColors(courses.length),
+      },
+    ],
+  };
+  //create options
+  const options = {};
   return (
-    <div>
-        <p>Visualise</p>
-        <div className='flex gap-x-5'>
-            <button
-            onClick={() => setCurrChart("students")}
-            >
-                Student
-            </button>
-            
-            <button
-            onClick={() => setCurrChart("income")}
-            >
-                Income
-            </button>
-        </div>
+    <div className="flex flex-1 flex-col gap-y-4 rounded-md bg-richblack-800 p-6">
+      <p className="text-lg font-bold text-richblack-5">Visualise</p>
+      <div className="space-x-4 font-semibold">
+        <button
+          onClick={() => setCurrChart("students")}
+          className={`rounded-sm p-1 px-3 transition-all duration-200 ${
+            currChart === "students"
+              ? "bg-richblack-700 text-yellow-50"
+              : "text-yellow-400"
+          }`}
+        >
+          Students
+        </button>
 
-        <div>
-                <Pie
-                    data={currChart === "students" ? chartDataForStudents : chartDataForIncome}
-                    options={options}
-                />
-            </div>
+        <button
+          onClick={() => setCurrChart("income")}
+          className={`rounded-sm p-1 px-3 transition-all duration-200 ${
+            currChart === "income"
+              ? "bg-richblack-700 text-yellow-50"
+              : "text-yellow-400"
+          }`}
+        >
+          Income
+        </button>
+      </div>
+      <div className="relative mx-auto aspect-square h-[325px] w-full place-items-center">
+        <Pie
+          data={
+            currChart === "students" ? chartDataForStudents : chartDataForIncome
+          }
+          options={options}
+        />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default InstructorChart
